a1664afd862dd98d79a4833dd8db82b611fbb8c8
egorWemakemvp <126478697+egorWemakemvp@users.noreply.github.com>
2023-04-11 15:25:58 +0300

    update index.js

--- index.js
+++ index.js
@@ -22,11 +22,44 @@ try {
 	if(!branch) throw Error("Branch not found");
 
     let apiUrl = "https://api.telegram.org/bot{botId}/sendMessage".replace("{botId}", botId);
+
+	const { eventName, workflow, job, runId, actor, ref, sha } = github.context;
+	const { owner, repo } = context.repo;
+	const shortRef = ref.replace(/^refs\/heads\//, '');
+	const shortSha = sha.slice(0, 7);
+
+	const a = escapeEntities(`${owner}/${repo}`);
+	const shortRefE = escapeEntities(shortRef);
+	const workflowE = escapeEntities(workflow);
+	const jobE = escapeEntities(job);
+	const actorE = escapeEntities(actor);
+	const eventNameE = escapeEntities(eventName);
+	const link = `https://github.com/${owner}/${repo}/actions/runs/${runId}`;
+	const text = `
+*${a}* ${shortRefE}\\(${shortSha}\\)
+${workflowE} ${jobE} ${status}
+Triggered by ${actorE} with a ${eventNameE} event
+[View details](${link})
+`;
 	axios.post(apiUrl, {
 		"chat_id": chatId,
-		"text": `${statuses[status]} Build ${status}, branch: ${branch}`,
+		"text": `${statuses[status]} Build ${status}, branch: ${branch}::` + text,
 		"parse_mode": "MarkdownV2"
 	}).then(r => console.log(r)).catch(e => console.log(e));
 } catch (error) {
   	core.setFailed(error.message);
+}
+
+function escapeEntities(input) {
+  const len = input.length;
+  let output = '';
+  for (let i = 0; i < len; i++) {
+    const c = input[i];
+    if (charsNeedEscape.indexOf(c) >= 0) {
+      output += '\\' + c;
+    } else {
+      output += c;
+    }
+  }
+  return output;
 }
